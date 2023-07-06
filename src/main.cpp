da94867bd81d293e54bb5fe037b2a2045e5549f3
Tristan Isham <tristan@isham.co>
2023-07-05 20:31:23 -0400

    main chaos

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include "server/server.hpp"
-#include "lang/lang.hpp"
 
 #ifdef NDEBUG
 const char VERSION[] = "v0.0.1";
@@ -15,13 +14,6 @@ int main(int argc, char** argv)
     if (first_arg == "--version" || first_arg == "-v") {
       std::cout << VERSION << std::endl;
       std::exit(0);
-    } else if (first_arg == "run") {
-      if (argc >= 3) {
-        std::string start_file_path{argv[2]};
-        return lang::run_file(start_file_path);
-      } else {
-        return lang::run_repl();
-      }
     }
 
     PORT = std::string { argv[1] };
